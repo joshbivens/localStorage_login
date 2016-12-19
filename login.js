1e90bede7489ccda29f64e3ec6729737c7822bb7
Josh Bivens <hotbioform@gmail.com>
2016-12-19 10:38:10 -0700

    Added selector const

--- login.js
+++ login.js
@@ -1,3 +1,10 @@
-const card = document.querySelector('.card');
-card.innerHTML = '<h3>Hello there!</h3>'
+const email = document.querySelector('input[name="email"]');
+const password = document.querySelector('input[name="password"]');
+const createEmail = document.querySelector('input[name="createEmail"]');
+const createPassword = document.querySelector('input[name="createPassword"]');
+const confirmPassword = document.querySelector('input[name="confirmPassword"]');
+
+
+
+
 
