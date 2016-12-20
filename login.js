6d450a525bc63991cb81c389d2a031bb31101fcb
Josh Bivens <hotbioform@gmail.com>
2016-12-20 10:14:02 -0700

    Made user object reusable

--- login.js
+++ login.js
@@ -4,22 +4,27 @@ const createEmail = document.querySelector('input[name="createEmail"]');
 const createPassword = document.querySelector('input[name="createPassword"]');
 const confirmPassword = document.querySelector('input[name="confirmPassword"]');
 const loginBtn = document.querySelector('#loginBtn');
+const loginDiv = document.querySelector('#login');
 const createBtn = document.querySelector('#createBtn');
 const createDiv = document.querySelector('#create');
 
+// Event Listeners 
 createBtn.addEventListener('click', createAccount);
 
+// Create Account Function
 function createAccount() {
+// Use Object.create and template strings to make resusable user objects
   let user = {
     email: createEmail.value,
     password: createPassword.value
-  }
+  };
 
   if(createPassword.value == confirmPassword.value) {
-    localStorage.setItem('user', JSON.stringify(user));
+    localStorage.setItem(`user${Date.now()}`, JSON.stringify(user));
     createDiv.style.display = 'none';
   } else {
     alert('Passwords do not match');
   }
 }
 
+// Login Function
